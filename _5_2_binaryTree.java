e628e9d947614161e103ca5008e54cf19bf9e9ec
rishav143 <rc8344353@gmail.com>
2024-01-23 10:45:18 +0530

    binary tree

--- _5_2_binaryTree.java
+++ _5_2_binaryTree.java
@@ -1,52 +1,117 @@
-class Node {
-    int data;
-    Node left, right;
-    
-    Node(int data) {
-        this.data = data;
-        left = null;
-        right = null;
-    }
-}
+// Binary Search Tree operations in Java
 
-class binaryTree {
+class BinarySearchTree {
+    class Node {
+      int key;
+      Node left, right;
+  
+      public Node(int item) {
+        key = item;
+        left = right = null;
+      }
+    }
+  
     Node root;
-    
-    void root(int data) {
-        root = insert(root, 1);
+  
+    BinarySearchTree() {
+      root = null;
     }
-    
-    Node insert(Node root, int key) {
-        if(root == null) {
-            return new Node(key);
-        } else if(root.data > key) {
-            root.left = insert(root.left, key);
-        } else {
-            root.right = insert(root.right, key);
-        }
+  
+    void insert(int key) {
+      root = insertKey(root, key);
+    }
+  
+    // Insert key in the tree
+    Node insertKey(Node root, int key) {
+      // Return a new node if the tree is empty
+      if (root == null) {
+        root = new Node(key);
         return root;
+      }
+  
+      // Traverse to the right place and insert the node
+      if (key < root.key)
+        root.left = insertKey(root.left, key);
+      else if (key > root.key)
+        root.right = insertKey(root.right, key);
+  
+      return root;
     }
-    
-    void inorder(Node root) {
-        if(root == null) {
-            return;
-        }
-        
-        inorder(root.left);
-        System.out.print(root.data + " ");
-        inorder(root.right);
+  
+    void inorder() {
+      inorderRec(root);
     }
-}
-
-public class _5_2_binaryTree
-{
-	public static void main(String[] args) {
-        binaryTree bt = new binaryTree();
-        
-        bt.root(1);
-        bt.insert(bt.root, 2);
-        bt.insert(bt.root, 3);
-        
-        bt.inorder(bt.root);
+  
+    // Inorder Traversal
+    void inorderRec(Node root) {
+      if (root != null) {
+        inorderRec(root.left);
+        System.out.print(root.key + " -> ");
+        inorderRec(root.right);
+      }
     }
-}
+  
+    void deleteKey(int key) {
+      root = deleteRec(root, key);
+    }
+  
+    Node deleteRec(Node root, int key) {
+      // Return if the tree is empty
+      if (root == null)
+        return root;
+  
+      // Find the node to be deleted
+      if (key < root.key)
+        root.left = deleteRec(root.left, key);
+      else if (key > root.key)
+        root.right = deleteRec(root.right, key);
+      else {
+        // If the node is with only one child or no child
+        if (root.left == null)
+          return root.right;
+        else if (root.right == null)
+          return root.left;
+  
+        // If the node has two children
+        // Place the inorder successor in position of the node to be deleted
+        root.key = minValue(root.right);
+  
+        // Delete the inorder successor
+        root.right = deleteRec(root.right, root.key);
+      }
+  
+      return root;
+    }
+  
+    // Find the inorder successor
+    int minValue(Node root) {
+      int minv = root.key;
+      while (root.left != null) {
+        minv = root.left.key;
+        root = root.left;
+      }
+      return minv;
+    }
+  
+    // Driver Program to test above functions
+    public static void main(String[] args) {
+      BinarySearchTree tree = new BinarySearchTree();
+  
+      tree.insert(8);
+      tree.insert(3);
+      tree.insert(1);
+      tree.insert(6);
+      tree.insert(7);
+      tree.insert(10);
+      tree.insert(14);
+      tree.insert(4);
+  
+      System.out.print("Inorder traversal: ");
+      tree.inorder();
+  
+      System.out.println("\n\nAfter deleting 10");
+      tree.deleteKey(10);
+      System.out.print("Inorder traversal: ");
+      tree.inorder();
+    }
+  }
